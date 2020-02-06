720648e601aafd14f0be85f45e3d19979ca9dc91
Bryan Dixon <javawolfpack@gmail.com>
2020-02-06 15:15:14 -0800

    Simple Model and Static Files

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Suggestion_Model(models.Model):
+    suggestion = models.CharField(max_length=240)
+    # author = models.CharField(max_length=240, default="bryan")
+
+    def __str__(self):
+        return self.suggestion 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request, page=0):
-    my_list=[]
-    for i in range(20):
-        i+=(page*10+1)
-        my_list+=[{
-            "first_name": "Name"+str(i),
-            "last_name": "Surname"+str(i),
-            "cat":"https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg"
-            }
-        ]
+    suggestion_list = models.Suggestion_Model.objects.all()
+
     context = {
         "title":"Tempate Demo",
         "body":"<p> Hello Body</p>",
-        "temp_list":my_list[0:10]
+        "suggestion_list":suggestion_list
     }
     return render(request, "index.html", context=context)
 
